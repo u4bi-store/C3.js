8421b62732e2e70c35f7b1f6880286e3330d094c
u4bi <rootcode10@gmail.com>
2016-09-23 17:22:26 +0900

    bar chart groups 바차트 특정 컬럼 그룹으로 묶어 표현하기

---
+++ bar2/bar2.js
@@ -0,0 +1,14 @@
+var chart = c3.generate({
+    bindto: "#chart",
+    data: {
+       columns: [
+            ['멋진애', 60,50],
+            ['안멋진애', 25,30],
+            ['어중간한애', 30,40]
+        ],
+        type: 'bar',
+        groups:[
+            ['멋진애', '어중간한애']
+        ]
+    }
+});
